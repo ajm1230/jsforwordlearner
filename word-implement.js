39613b91420b06e279fcd427da4e8f12dafea020
ajm1230 <155264455+ajm1230@users.noreply.github.com>
2024-02-03 19:43:46 +0530

    Add files via upload

---
+++ word-implement.js
@@ -0,0 +1,12 @@
+
+    const words = [
+      { word: "Hello", meaning: "नमस्ते (Namesta) ", definition: "A respectful greeting in Hindi.", example: "नमेस्ता, कैसे है आप ? ( Hello, how are you? )" },
+      { word: "Thank You", meaning: "धन्यवाद (Dhanyavaad)", definition: "Thank you in Hindi.", example: "शुक्रिया बहुत बहुत!" },
+      { word: "Guest", meaning: "मेहमान (Mehmaan)", definition: "Guest in Hindi.", example: "आज हमारे घर में मेहमान आए हैं।" },
+      { word: "Market", meaning: "बाजार (Bazaar)", definition: "Market in Hindi.", example: "मैंने आज बाजार से सब्जियाँ खरीदी।" },
+      { word: "Attention", meaning: "ध्यान (Dhyaan)", definition: "Attention or focus in Hindi.", example: "कृपया ध्यान से सुनिए।" },
+      { word: "Condition ", meaning: "हाल (Haal)", definition: "Condition or state in Hindi.", example: "आपका हाल कैसा है?" }
+    ];
+
+    let currentIndex = 0; // Initialize the index to 0 to display the first word
+    
